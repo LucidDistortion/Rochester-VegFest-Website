d1ee28bed3e79641b60ad7106c675037831d13cf
Graham Home <grahamhome333@gmail.com>
2017-06-15 23:00:40 -0400

    Footer improved

--- scripts/input_resize.js
+++ scripts/input_resize.js
@@ -1,6 +1,7 @@
 function resize() {
 	var width = document.getElementById("emailForm").offsetWidth;
-	document.getElementById("emailInput").size = width/10;
+	document.getElementById("emailInput").size = (width-125)/10;
+	console.log(document.body.clientWidth);
 }
 window.onload = resize;
 window.onresize = resize;
