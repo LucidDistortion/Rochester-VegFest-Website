f7194591a0643a6d0548b54080e25c69a5c93014
Graham Home <grahamhome333@gmail.com>
2017-06-18 14:04:27 -0400

    Added thank-you and confirmation pages for Subscription forms.

--- scripts/input_resize.js
+++ scripts/input_resize.js
@@ -1,6 +1,6 @@
 function resize() {
-	var width = document.getElementById("emailForm").offsetWidth;
-	document.getElementById("emailInput").size = (width-125)/10;
+	var width = document.getElementById("mc_embed_signup").offsetWidth;
+	document.getElementById("mce-EMAIL").size = (width-125)/10;
 	console.log(document.body.clientWidth);
 }
 window.onload = resize;
